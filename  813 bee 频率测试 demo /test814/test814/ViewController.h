48f148c2bae78ea28591a6ef922681195cd7c014
“zhuhong216376” <zhuhong216376@sogou-inc.com>
2018-08-14 11:59:55 +0800

    oc swift混编

---
+++  813 bee 频率测试 demo /test814/test814/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test814
+//
+//  Created by nemo on 2018/8/14.
+//  Copyright © 2018年 搜狗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++  813 bee 频率测试 demo /test814/test814/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  test813
+//
+//  Created by nemo on 2018/8/13.
+//  Copyright © 2018年 搜狗. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Beethoven/Beethoven-Swift.h>
+@interface ViewController () <PitchEngineDelegate>
+@property (nonatomic ,strong) PitchEngine *pitchEgine;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.pitchEgine = [[PitchEngine alloc]initWithDelegate:self];
+    //    self.pitchEgine.active
+    [self test];
+    
+}
+
+
+- (void)test
+{
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    [self.view addSubview:btn];
+    btn.backgroundColor = [UIColor redColor];
+    [btn setTitle:@"开始" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(startEngine) forControlEvents:UIControlEventTouchUpInside];
+    
+}
+
+- (void)startEngine
+{
+    [self.pitchEgine start];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (void)pitchEngine:(PitchEngine * _Nonnull)pitchEngine withfrequency:(float)withfrequency {
+    NSLog(@"频率频率%f",withfrequency);
+}
+
+
+@end
